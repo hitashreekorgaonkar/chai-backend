ae4f8854be2e04ab2045fd591bd2a36957088fbf
hitashreekorgaonkar <hkorgaonkar10@gmail.com>
2024-08-09 18:28:41 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,24 +1,32 @@
-import express from express
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
-
 // NOTE: Explore cors options and its role {} in docs
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials:true
-}))
+    credentials: true,
+  })
+);
 
-// how much limit you want to give? as per you server power 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+// how much limit you want to give? as per you server power
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 // urlencoded is used for url eg. %20-space // The “extended” syntax allows giving {} inside {} / nested {}
-app.use(express.static("public"))
+app.use(express.static("public"));
 // It serve static assets/files for storing files, folders like pdf, img
 
-app.use(cookieParser())
+app.use(cookieParser());
 // ep.8 18:25 From my server to users browser cookie to access and set basically CRUD operation, secure cookies which server can only read and remove
 
-export { app }
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routers declaration ep. 12 10:03
+app.use("/api/v1/users", userRouter);
+// http://localhost:8000/api/v1/users/register
+
+export { app };
